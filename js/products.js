71692dec64e5a0c57743a9f1c8948114048c1263
ernestoabreu98 <64290330+ernestoabreu98@users.noreply.github.com>
2020-08-23 19:15:55 -0300

    Range Filter
    Signed-off-by: ernestoabreu98 <64290330+ernestoabreu98@users.noreply.github.com>

--- js/products.js
+++ js/products.js
@@ -133,7 +133,7 @@ document.addEventListener("DOMContentLoaded", function(e){
     document.getElementById("rangeFilterCount").addEventListener("click", function(){
         //Obtengo el mínimo y máximo de los intervalos para filtrar por cantidad
         //de productos por categoría.
-        alert("Algo no anda Bien")
+        alert("Algo no anda Bien");
         minCount = document.getElementById("rangeFilterCountMin").value;
         maxCount = document.getElementById("rangeFilterCountMax").value;
 
