e467546aea75a2e3b48fbdde0c35179224945f02
ernestoabreu98 <64290330+ernestoabreu98@users.noreply.github.com>
2020-08-23 19:11:14 -0300

    Range Filter
    Signed-off-by: ernestoabreu98 <64290330+ernestoabreu98@users.noreply.github.com>

--- js/products.js
+++ js/products.js
@@ -58,8 +58,7 @@ function showProductsList(){
     for(let i = 0; i < currentProductsArray.length; i++){
         let products = currentProductsArray[i];
 
-        if (((minCount == undefined) || (minCount != undefined && parseInt(products.soldCount) >= minCount)) &&
-            ((maxCount == undefined) || (maxCount != undefined && parseInt(products.soldCount) <= maxCount))){
+        if (((minCount == undefined)) && ((maxCount == undefined))){
 
             htmlContentToAppend += `
             <a href="product-info.html" class="list-group-item list-group-item-action">
@@ -149,7 +148,7 @@ document.addEventListener("DOMContentLoaded", function(e){
         else{
             maxCount = undefined;
         }
-
+        
         showProductsList();
     });
 });
