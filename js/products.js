4f5c7dd63806887e3d6494a4c216038f5830340a
ernestoabreu98 <64290330+ernestoabreu98@users.noreply.github.com>
2020-08-23 13:58:38 -0300

    Sort By Price 7
    Signed-off-by: ernestoabreu98 <64290330+ernestoabreu98@users.noreply.github.com>

--- js/products.js
+++ js/products.js
@@ -1,7 +1,7 @@
 const ORDER_ASC_BY_NAME = "AZ";
 const ORDER_DESC_BY_NAME = "ZA";
-const ORDER_ASC_BY_PRICE = "Precio-ASC";
-const ORDER_DESC_BY_PRICE = "Precio-DESC";
+const ORDER_ASC_BY_PRICE = "ORDER_ASC_BY_PRICE";
+const ORDER_DESC_BY_PRICE = "ORDER_DESC_BY_PRICE";
 const ORDER_BY_PROD_COUNT = "Cant.";
 var currentProductsArray = [];
 var currentSortCriteria = undefined;
@@ -25,8 +25,13 @@ function sortProducts(criteria, array){
         });
         
     }else if (criteria === ORDER_ASC_BY_PRICE){
-        result = array.sort();
-    } else if (criteria === ORDER_DESC_BY_PRICE){
+        result = array.sort(function(a, b) {
+            if ( a.cost > b.cost){ return 1; }
+            if ( a.cost < b.cost){ return -1; }
+            return 0;
+        });
+        
+    }else if (criteria === ORDER_DESC_BY_PRICE){
         result = array.sort(function(a, b) {
             if ( a.cost > b.cost ){ return -1; }
             if ( a.cost < b.cost ){ return 1; }
