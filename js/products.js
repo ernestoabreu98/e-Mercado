438b3126193904b8d3526b041c6e307cff6fc997
ernestoabreu98 <64290330+ernestoabreu98@users.noreply.github.com>
2020-08-23 13:45:06 -0300

    Sort By Price 4
    Signed-off-by: ernestoabreu98 <64290330+ernestoabreu98@users.noreply.github.com>

--- js/products.js
+++ js/products.js
@@ -26,8 +26,8 @@ function sortProducts(criteria, array){
         
     }else if (criteria === ORDER_ASC_BY_PRICE){
         result = array.sort(function(a, b) {
-            if ( a.cost > b.cost ){ return -1; }
-            if ( a.cost < b.cost ){ return 1; }
+            if ( a.cost < b.cost ){ return -1; }
+            if ( a.cost > b.cost ){ return 1; }
             return 0;
         });
     }else if (criteria === ORDER_DESC_BY_PRICE){
